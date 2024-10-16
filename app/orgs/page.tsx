aa37c32614fcbf7126a82c1a31566a179b647f8e
Lyhong Peou <102566014+LyhongPeou@users.noreply.github.com>
2024-10-16 02:51:56 -0400

    Added list of orgs

--- app/orgs/page.tsx
+++ app/orgs/page.tsx
@@ -1,11 +1,31 @@
-function OrganizationPage(){
+import { createClient } from '@/utils/supabase/server'
+
+
+async function OrganizationPage(){
+
+    const supabase = createClient()
+
+    let { data: organizations, error } = await supabase
+    .from("organizations")
+    .select("*");
+
+
     return <> 
     
     <p> Organization Page</p>
+
+
+    <ul>
+        {organizations?.map((org) => (
+          <li key={org.id}>{org.name} - {org.address} - {org.city} - {org.state_region} </li>
+        ))}
+      </ul>
     
     </>
 }
 
 export default OrganizationPage
+
+export const cache = 'no-store'; // Prevent caching
 
 
